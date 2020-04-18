b4661865e988971008559e1962790fc93b26bb05
Aman-am <amanmehta015@gmail.com>
2020-04-19 01:09:56 +0530

    Minimum Path sum DP optimised

--- leetcode/minimumPathSum.cpp
+++ leetcode/minimumPathSum.cpp
@@ -1,17 +1,25 @@
-// https://leetcode.com/problems/number-of-islands/
+// https://leetcode.com/problems/minimum-path-sum/
 
 #include <iostream>
 #include <bits/stdc++.h>
-#define fo(i, n) for(i=0; i<n; i++)
+#define fo(i, k, n) for(i=k; i<=n; i++)
 
 using namespace std;
 
 int findMinPathSum(vector<vector<int>>& grid, int r, int c) {
     int i, j, count=0;
-    if(r<0 || c<0)return INT_MAX;
-    else if(r==0 && c==0) return grid[r][c];
-    else return grid[r][c] + min(findMinPathSum(grid,r-1,c),findMinPathSum(grid,r,c-1));
+    // Total cost array
+    int tc[r+1][c+1];
+    tc[0][0] = grid[0][0];
+    fo(i,1,c)   tc[0][i] = tc[0][i-1] + grid[0][i];
+    fo(i,1,r)   tc[i][0] = tc[i-1][0] + grid[i][0];
+
+    fo(i,1,r)
+        fo(j,1,c) tc[i][j] = grid[i][j] + min(tc[i][j-1], tc[i-1][j] );
+
+    return tc[r][c];
 }
+
 int minPathSum(vector<vector<int>>& grid) {
     int r= grid.size(), c=0;
     if(r) c=grid[0].size();
