af38c7b814b964ef31ded6ae7a0b3b0640dd4a56
Aman-am <amanmehta015@gmail.com>
2020-06-03 01:43:18 +0530

    Invert Binary Tree - Queue Based

--- leetcode/invertBinaryTree.cpp
+++ leetcode/invertBinaryTree.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+// Recursive Implementation
     TreeNode* invertTree(TreeNode* root) {
         if (root == nullptr)
             return nullptr;
@@ -19,6 +20,26 @@ public:
         root->left = temp;
         invertTree(root->left);
         invertTree(root->right);
+        return root;
+    }
+
+// Queue Implementation
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr)
+            return nullptr;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            TreeNode* current = q.front();
+            q.pop();
+            TreeNode* temp = current->right;
+            current->right = current->left;
+            current->left = temp;
+            if(current->left!= nullptr) q.push(current->left);
+            if(current->right!= nullptr) q.push(current->right);
+        }
         return root;
     }
 };
